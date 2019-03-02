0e9562590530e19158f7b3858cc480c0258f0771
SkynetDev <andrejohnson0017@gmail.com>
2019-03-02 09:37:06 -0500

    updated html and javascript documents

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var wins;
+var losses;
+var guesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Create a prompt for users input
+var usersGuess = prompt("Choose a letter for your guess");
+
+// Converts user inputs to lowercase in order to match lowercase elements in the array
+var usersGuessL = userGuess.toLowerCase();
+
+// Choosing random array element for computer guess
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+
+
+
+
